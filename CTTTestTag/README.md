0747cbda791cbb0b5695f9290604450efa253f74
Thorsten von Eicken <tve@voneicken.com>
2024-08-10 16:38:30 -0700

    ComboTestTag: initial version

--- CTTTestTag/README.md
+++ CTTTestTag/README.md
@@ -6,3 +6,7 @@ This code uses Arduino RadioLib to program a common integrated FSK radio transce
 Boards supported:
 - Adafruit Feather 32u4 RFM69HCW (product ID 3077)
 - Possibly: Adafruit Feather M0 RFM69HCW (product ID 3177)
+
+ Be sure to use the 433Mhz version of these boards, not the 915Mhz one, although it's likely to
+ work too...
+ 
---
+++ ComboTestTag/build.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -e
+# Build the firsmware from scratch, prereq: arduino-cli installed
+# Lots of stuff copied from https://github.com/adafruit/ci-arduino/blob/master/install.sh
+
+export ARDUINO_BOARD_MANAGER_ADDITIONAL_URLS=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
+set nonomatch=1
+
+if ! which arduino-cli >/dev/null; then echo "Please install arduino-cli"; exit 1; fi
+
+if ! [[ -d $HOME/.arduino15/packages/adafruit ]]; then
+    arduino-cli core install adafruit:samd
+fi
+
+if ! [[ -f $HOME/.arduino15/library_index.json ]]; then
+	arduino-cli lib update-index
+fi
+arduino-cli lib install RadioLib@6.6.0 CRC@1.0.3 "Arduino Low Power@1.2.2"
+
+# if ! [[ -x ../uf2conv/uf2conv ]]; then
+#     ( cd ../uf2conv; cc -o uf2conv uf2conv.c)
+# fi
+
+arduino-cli compile --fqbn adafruit:samd:adafruit_feather_m0_express
+
+if [[ -d /run/media/$USER/FEATHERBOOT ]]; then
+    echo "Uploading"
+    cp firmware.uf2 /run/media/$USER/FEATHERBOOT
+fi
